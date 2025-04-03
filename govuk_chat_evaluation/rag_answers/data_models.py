e05262d85df69f62e1274fa4e55a3509533e260d
Kevin Dew <kevindew@me.com>
2025-04-03 09:28:10 +0100

    Very basic Deepeval integration
    This starts the process of integrating with Deepeval by running the code
    to do a very contrived evaluation where we don't do anything with the
    results.
    
    I unfortunately had to set Pyright to ignore the typing for the
    deepeval_evaluate function because of a typing issue in deepeval [1]
    of:
    
    ```
    /Users/kevin.dew/dev/govuk-chat-evaluation-prototype/govuk_chat_evaluation/rag_answers/evaluate.py
      /Users/kevin.dew/dev/govuk-chat-evaluation-prototype/govuk_chat_evaluation/rag_answers/evaluate.py:31:23 - error: Argument of type "list[LLMTestCase]" cannot be assigned to parameter "test_cases" of type "List[LLMTestCase | MLLMTestCase] | List[ConversationalTestCase]" in function "evaluate"
        Type "list[LLMTestCase]" is not assignable to type "List[LLMTestCase | MLLMTestCase] | List[ConversationalTestCase]"
          "list[LLMTestCase]" is not assignable to "List[LLMTestCase | MLLMTestCase]"
            Type parameter "_T@list" is invariant, but "LLMTestCase" is not the same as "LLMTestCase | MLLMTestCase"
            Consider switching from "list" to "Sequence" which is covariant
          "list[LLMTestCase]" is not assignable to "List[ConversationalTestCase]"
            Type parameter "_T@list" is invariant, but "LLMTestCase" is not the same as "ConversationalTestCase"
            Consider switching from "list" to "Sequence" which is covariant (reportArgumentType)
    1 error, 0 warnings, 0 informations
    ```
    
    [1]: https://github.com/confident-ai/deepeval/pull/953

--- govuk_chat_evaluation/rag_answers/data_models.py
+++ govuk_chat_evaluation/rag_answers/data_models.py
@@ -1,3 +1,4 @@
+from deepeval.test_case import LLMTestCase
 from pydantic import BaseModel
 
 
@@ -7,3 +8,10 @@ class EvaluationTestCase(BaseModel):
     ideal_answer: str
     llm_answer: str
     # TODO: lots more data fields
+
+    def to_llm_test_case(self) -> LLMTestCase:
+        return LLMTestCase(
+            input=self.question,
+            expected_output=self.ideal_answer,
+            actual_output=self.llm_answer,
+        )
