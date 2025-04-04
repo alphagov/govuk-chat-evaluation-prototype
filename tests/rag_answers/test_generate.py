f81a7719a47ce9aab8c951fd0e7156e26b499b46
Kevin Dew <kevindew@me.com>
2025-04-04 19:37:45 +0100

    Mechanism to generate rag answers
    This adds the code to interface with the Ruby rake task in order to
    generate an answer. It is quite similar to the code of the same purpose
    for jailbreak_guardrails.
    
    It's pertinent to note that this is likely the absolute minimum of data
    for usage. I expect to use this properly we'll have to consider a more
    complex data structure and likely validate it (so probably need a
    distinct Pydantic model).

---
+++ tests/rag_answers/test_generate.py
@@ -0,0 +1,66 @@
+import json
+from unittest.mock import AsyncMock
+
+import pytest
+
+from govuk_chat_evaluation.rag_answers.generate import (
+    generate_inputs_to_evaluation_test_cases,
+    generate_and_write_dataset,
+    GenerateInput,
+    EvaluationTestCase,
+)
+
+
+@pytest.fixture
+def run_rake_task_mock(mocker):
+    mock = mocker.patch(
+        "govuk_chat_evaluation.rag_answers.generate.run_rake_task",
+        new_callable=AsyncMock,
+    )
+    mock.side_effect = lambda *_: {"message": "An answer"}
+    return mock
+
+
+@pytest.mark.usefixtures("run_rake_task_mock")
+def test_generate_models_to_evaluation_test_cases_returns_evaluation_test_cases():
+    generate_inputs = [
+        GenerateInput(question="Question 1", ideal_answer="Answer 1"),
+        GenerateInput(question="Question 2", ideal_answer="Answer 2"),
+    ]
+    expected_results = [
+        EvaluationTestCase(
+            question="Question 1", ideal_answer="Answer 1", llm_answer="An answer"
+        ),
+        EvaluationTestCase(
+            question="Question 2", ideal_answer="Answer 2", llm_answer="An answer"
+        ),
+    ]
+    actual_results = generate_inputs_to_evaluation_test_cases("openai", generate_inputs)
+
+    assert sorted(expected_results, key=lambda r: r.question) == sorted(
+        actual_results, key=lambda r: r.question
+    )
+
+
+def test_generate_models_to_evaluation_test_cases_runs_expected_rake_task(
+    run_rake_task_mock,
+):
+    run_rake_task_mock.side_effect = lambda *_: {"message": "An answer"}
+    generate_inputs = [
+        GenerateInput(question="Question 1", ideal_answer="Answer"),
+    ]
+    generate_inputs_to_evaluation_test_cases("openai", generate_inputs)
+
+    run_rake_task_mock.assert_called_with(
+        "evaluation:generate_rag_structured_answer_response[openai]",
+        {"INPUT": "Question 1"},
+    )
+
+
+@pytest.mark.usefixtures("run_rake_task_mock")
+def test_generate_and_write_dataset(mock_input_data, mock_project_root):
+    path = generate_and_write_dataset(mock_input_data, "openai", mock_project_root)
+    assert path.exists()
+    with open(path, "r") as file:
+        for line in file:
+            assert json.loads(line)
